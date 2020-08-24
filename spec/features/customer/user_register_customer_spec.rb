8baf1ef1b9e9869132271cd995d348a67ce6059e
João Victor <victorpow@live.com>
2020-08-24 01:55:44 -0300

    user register a new customer - test feature

---
+++ spec/features/customer/user_register_customer_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User create costumer' do
+  scenario 'Successfully' do
+    user = create(:user)
+
+    login_as(user, scope: :user)
+    visit root_path
+    click_on 'Clientes'
+    click_on 'Registrar Novo'
+    fill_in 'Nome', with: 'Douglas Adams'
+    fill_in 'E-mail', with: 'douglas@gmail.com'
+    fill_in 'Telefone', with: '(42) 4242-4242'
+    click_on 'Salvar'
+
+    expect(page).to have_content('Douglas Adams')
+    expect(page).to have_content('douglas@gmail.com')
+    expect(page).to have_content('(42) 4242-4242')
+  end
