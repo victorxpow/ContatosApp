0e25b6a6f0d6fe01913b7d38021956667a266e74
João Victor <victorpow@live.com>
2020-08-24 02:42:11 -0300

    customer register feature done

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,4 +8,23 @@ class CustomersController < ApplicationController
   def new
     @customer = Customer.new
   end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to @customer, notice: 'Registrado com sucesso.'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :phone)
+  end
 end
