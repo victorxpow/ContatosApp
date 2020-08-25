788e827e9f1f05bc5b298f9027e3eb47cffe31c9
João Victor <victorpow@live.com>
2020-08-25 02:17:51 -0300

    add some style

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,4 @@
 class Contact < ApplicationRecord
-  has_many :customer_contacts
-  has_many :customers, through: :customer_contacts
+  has_many :customer_contacts, dependent: :destroy
+  has_many :customers, through: :customer_contacts, dependent: :destroy
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
 class Customer < ApplicationRecord
-  has_many :customer_contacts
-  has_many :contacts, through: :customer_contacts
+  has_many :customer_contacts, dependent: :destroy
+  has_many :contacts, through: :customer_contacts, dependent: :destroy
 end
